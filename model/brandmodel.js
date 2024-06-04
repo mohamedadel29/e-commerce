1910b33e8950f1a32bd3df0fef0a99c60a132645
mohamedadel29 <amohamedadel777@gmail.com>
2024-06-04 16:30:49 +0300

    brand edit

--- model/brandmodel.js
+++ model/brandmodel.js
@@ -19,23 +19,23 @@ const brandSchema = new mongoose.Schema(
 );
 
 
-const setImageURL = (doc) => {
-  if (doc.image) {
-    let imageUrl = `http://localhost:${process.env.PORT}/`;
-    process.env.NODE_ENV == 'production' &&
-			(imageUrl = process.env.STATIC_CONTENT_SERVER_HOST);
-    doc.image = imageUrl + 'brands/' + doc.image;
-  }
-};
+// const setImageURL = (doc) => {
+//   if (doc.image) {
+//     let imageUrl = `http://localhost:${process.env.PORT}/`;
+//     process.env.NODE_ENV == 'production' &&
+// 			(imageUrl = process.env.STATIC_CONTENT_SERVER_HOST);
+//     doc.image = imageUrl + 'brands/' + doc.image;
+//   }
+// };
 
-// findOne, findAll and update
-brandSchema.post('init', (doc) => {
-  setImageURL(doc);
-});
+// // findOne, findAll and update
+// brandSchema.post('init', (doc) => {
+//   setImageURL(doc);
+// });
 
-// create
-brandSchema.post('save', (doc) => {
-  setImageURL(doc);
-});
+// // create
+// brandSchema.post('save', (doc) => {
+//   setImageURL(doc);
+// });
 
 module.exports = mongoose.model('Brand', brandSchema);
